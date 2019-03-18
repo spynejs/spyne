c0cee56ee1690948ab67008bd34d9e2681dbce49
Frank Batista <nybatista@aol.com>
2019-03-18 15:15:09 -0400

    renamed sendViewStreamInfo to sendInfoToChannel

--- src/spyne/utils/mixins/base-streams-mixins.js
+++ src/spyne/utils/mixins/base-streams-mixins.js
@@ -15,7 +15,7 @@ export function baseStreamsMixins() {
     sendUIPayload: function(obs, data) {
       return new ViewStreamPayload('CHANNEL_UI', obs, data, 'subscribe');
     },
-    sendViewStreamInfo: function(channelName, payload) {
+    sendInfoToChannel: function(channelName, payload) {
       const getProp = str => R.prop(str, this.props);
       const channel = channelName;
       let srcElement = {
--- src/spyne/views/view-stream.js
+++ src/spyne/views/view-stream.js
@@ -803,7 +803,7 @@ export class ViewStream {
    * @example
    * let payload = {'location' : 'about'};
    * let action = 'PAGE_CHANGE_EVENT';
-   * this.sendViewStreamInfo('ROUTE', paylaod, action);
+   * this.sendInfoToChannel('ROUTE', paylaod, action);
    *
    * */
 
@@ -816,7 +816,7 @@ export class ViewStream {
     return channelExists;
   }
 
-  sendViewStreamInfo(channelName, payload = {},  action="VIEWSTREAM_EVENT") {
+  sendInfoToChannel(channelName, payload = {},  action="VIEWSTREAM_EVENT") {
     let data = {payload, action};
     data['srcElement'] = {};// R.pick(['cid','viewName'], data);
     data.srcElement['cid'] = R.path(['props','cid'],this);;
@@ -853,9 +853,9 @@ export class ViewStream {
 
 
     if (isRendered === true){
-      this.sendViewStreamInfo('CHANNEL_LIFECYCLE', {action:'CHANNEL_LIFECYCLE_RENDERED_EVENT'}, 'CHANNEL_LIFECYCLE_RENDERED_EVENT');
+      this.sendInfoToChannel('CHANNEL_LIFECYCLE', {action:'CHANNEL_LIFECYCLE_RENDERED_EVENT'}, 'CHANNEL_LIFECYCLE_RENDERED_EVENT');
     } else if (isDisposed === true){
-       this.sendViewStreamInfo('CHANNEL_LIFECYCLE', {action:'CHANNEL_LIFECYCLE_REMOVED_EVENT'}, 'CHANNEL_LIFECYCLE_REMOVED_EVENT');
+       this.sendInfoToChannel('CHANNEL_LIFECYCLE', {action:'CHANNEL_LIFECYCLE_REMOVED_EVENT'}, 'CHANNEL_LIFECYCLE_REMOVED_EVENT');
     }
 
 
