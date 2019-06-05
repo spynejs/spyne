d09f31c48f05e8fb3b7bb3d55d6c56641b2e376a
Frank Batista <nybatista@aol.com>
2019-06-05 15:00:05 -0400

    merging pull request

--- src/spyne/spyne.js
+++ src/spyne/spyne.js
@@ -34,7 +34,7 @@ class SpyneApp {
    */
   constructor(config = {}) {
     this.channels = new ChannelsController();
-    this.VERSION = '0.10.16';
+    this.VERSION = '0.10.17';
     this.ViewStream = ViewStream;
     this.BasicView = ViewStreamElement;
     this.DomEl = DomEl;
