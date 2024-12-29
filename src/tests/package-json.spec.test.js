c9a293b91809e024cb759624f9403aa3afa5a551
Frank Batista <nybatista@gmail.com>
2024-12-28 23:47:41 -0500

    committing test of husky test

--- src/tests/package-json.spec.test.js
+++ src/tests/package-json.spec.test.js
@@ -4,7 +4,7 @@ import pkg from '../../package.json' // Import package.json
 describe('package.json export import statement', () => {
   it('should be set to "./lib/spyne.esm.js"', () => {
     const exports = pkg.exports['.']
-    expect(pkg.main).to.equal('./lib/spyne.esm5.js')
+    expect(pkg.main).to.equal('./lib/spyne.esm.js')
     expect(pkg.module).to.equal('./lib/spyne.esm.js')
     expect(exports.import).to.equal('./lib/spyne.esm.js')
     expect(exports.require).to.equal('./lib/spyne.esm.js')
