fcecc9a90cdace7bb720fdd51431d27fd37faf51
Frank Batista <nybatista@gmail.com>
2026-04-28 11:33:15 -0400

    updated karma config to resolve github-ci headless chrome issue

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,18 @@
 const replace = require('@rollup/plugin-replace');
 const dotenv = require('dotenv');
+
 dotenv.config();
 
-module.exports = function (config) {
-  // ...
-  // Use process.env.IS_PUBLIC as needed
-  // ...
-};
+const isCI = Boolean(
+    process.env.GITHUB_ACTIONS ||
+    process.env.TRAVIS ||
+    process.env.CI
+);
 
 module.exports = function (config) {
-
-  if (process.env.TRAVIS || process.env.GITHUB_ACTIONS || process.env.CI) {
-    config.browsers = ['ChromeHeadlessNoSandbox'];
-  }
-
   config.set({
     frameworks: ['mocha', 'chai'],
 
-    // Load these plugins so Karma recognizes 'rollup' as a preprocessor
     plugins: [
       'karma-mocha',
       'karma-chai',
@@ -26,20 +21,18 @@ module.exports = function (config) {
     ],
 
     files: [
-      // Test files
       { pattern: './node_modules/ramda/dist/ramda.min.js', watched: false },
       { pattern: './node_modules/rxjs/**/*.js', included: false, watched: false },
 
-      // Your test files:
       { pattern: './src/tests/index.test.js', watched: true },
       { pattern: './src/tests/spyne-app.test.js', watched: true },
       { pattern: './src/tests/package-json.spec.test.js', watched: true },
       { pattern: './src/tests/channels/*.test.js', watched: true },
       { pattern: './src/tests/utils/*.test.js', watched: true },
-      { pattern: './src/tests/views/*.test.js', watched: true }    ],
+      { pattern: './src/tests/views/*.test.js', watched: true }
+    ],
 
     preprocessors: {
-      // We want to run Rollup on our test files (and any imports they pull in)
       './src/tests/*.test.js': ['rollup'],
       './src/tests/channels/*.test.js': ['rollup'],
       './src/tests/utils/*.test.js': ['rollup'],
@@ -52,28 +45,48 @@ module.exports = function (config) {
         name: 'SpyneTest',
         sourcemap: 'inline'
       },
+
       plugins: [
         replace({
-          // The replacement you want to do:
           'process.env.IS_PUBLIC': JSON.stringify(process.env.IS_PUBLIC === 'true'),
-          // Or if you'd rather fallback to 'false' if not set:
-          // 'process.env.IS_PUBLIC': JSON.stringify(process.env.IS_PUBLIC ?? 'false'),
-
           preventAssignment: true
         }),
 
-        require('@rollup/plugin-node-resolve').default(),
+        require('@rollup/plugin-node-resolve').default({
+          browser: true
+        }),
+
         require('@rollup/plugin-commonjs')({
           transformMixedEsModules: true
         }),
+
         require('@rollup/plugin-json')()
       ]
     },
 
-    // Browsers
-    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--disable-setuid-sandbox',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
 
-    // run once and exit or watch
-    singleRun: true
+    browsers: [isCI ? 'ChromeHeadlessNoSandbox' : 'Chrome'],
+
+    singleRun: true,
+
+    autoWatch: false,
+
+    browserNoActivityTimeout: 60000,
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 2,
+
+    reporters: ['progress']
   });
 };
